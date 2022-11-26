6afaf4f0f24956798ae4536bf7a80214d018c744
Tassio Luiz D. Carmo <tassiol.carmo@gmail.com>
2022-11-25 21:57:50 -0300

    Update 13.sql

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,3 @@
+-- query to list the names of all people who starred in a movie in which Kevin Bacon also starred. 
 SELECT peopleB.name FROM (SELECT movie_id AS A_movie_id FROM people peopleA JOIN stars starsA ON peopleA.id = starsA.person_id WHERE peopleA.name == 'Kevin Bacon' AND peopleA.birth == 1958)
 JOIN stars starsB ON A_movie_id = starsB.movie_id JOIN people peopleB ON peopleB.id = starsB.person_id WHERE peopleB.name <> 'Kevin Bacon' OR peopleB.birth <> 1958 GROUP BY peopleB.name;
