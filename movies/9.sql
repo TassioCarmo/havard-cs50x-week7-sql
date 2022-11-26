820f36d25e5ef7e54490a9a8afd0697ef2435678
Tassio Luiz D. Carmo <tassiol.carmo@gmail.com>
2022-11-25 21:56:33 -0300

    Update 9.sql

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,2 @@
+--query to list the names of all people who starred in a movie released in 2004, ordered by birth year. 
 SELECT name from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id  where movies.year == 2004 ORDER by birth;
