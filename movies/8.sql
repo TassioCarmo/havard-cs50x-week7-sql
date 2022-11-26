18d75f9dc0ad0d33fed6cd8a1bf85989feadc594
Tassio Luiz D. Carmo <tassiol.carmo@gmail.com>
2022-11-25 21:56:14 -0300

    Update 8.sql

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,2 @@
+-- query to list the names of all people who starred in Toy Story. 
 SELECT name from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = "Toy Story";
