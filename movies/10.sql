6eeb24af87b53298c044bdff1c19454012ec8620
Tassio Luiz D. Carmo <tassiol.carmo@gmail.com>
2022-11-25 21:56:59 -0300

    Update 10.sql

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,2 @@
+-- query to list the names of all people who have directed a movie that received a rating of at least 9.0. 
 SELECT name FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN directors ON movies.id = directors.movie_id JOIN people ON directors.person_id = people.id WHERE rating >= 9.0 GROUP BY name;
