146a73fc9062ff89eca92df82261202a1baff0a1
Tassio Luiz D. Carmo <tassiol.carmo@gmail.com>
2022-11-25 21:57:32 -0300

    Update 12.sql

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,2 @@
+-- query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred. 
 SELECT title FROM (SELECT title, COUNT(title) AS n FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE name == "Johnny Depp" OR name == "Helena Bonham Carter" GROUP BY title) WHERE n == 2;
